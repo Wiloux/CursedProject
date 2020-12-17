bdfc33c65a874e29d20697520791e29266d138c9
Wiloux <54811449+Wiloux@users.noreply.github.com>
2020-12-17 18:37:59 +0100

    Basic movements
    W & S to move forward/backward ; rotate the character on the X axis by moving the mouse

---
+++ Assets/Player_Movement.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    float rotX;
+    public CharacterController _characterController;
+    public float _rotSpeed = 20;
+    public float _verticalSpeed = 10f;
+    public float _verticalSpeedNeg = 5f;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RotatePlayerWithMouse();
+        Movement();
+    }
+
+
+    void Movement()
+    {
+        float verticalMove = 0;
+        Vector3 moveDirection;
+        if (Input.GetAxis("Vertical") > 0.1)
+        {
+            verticalMove = Input.GetAxis("Vertical") * Time.deltaTime * _verticalSpeed;
+            moveDirection = transform.TransformDirection(Vector3.forward) * verticalMove;
+            _characterController.Move(moveDirection);
+
+        }
+
+        if (Input.GetAxis("Vertical") < -0.1)
+        {
+            verticalMove = Input.GetAxis("Vertical") * Time.deltaTime * _verticalSpeedNeg;
+            moveDirection = transform.TransformDirection(Vector3.forward) * verticalMove;
+            _characterController.Move(moveDirection);
+        }
+    }
+
+    void RotatePlayerWithMouse()
+    {
+        rotX = Input.GetAxis("Mouse X") * _rotSpeed * Mathf.Deg2Rad;
+        transform.Rotate(Vector3.up, rotX);
+    }
+}
