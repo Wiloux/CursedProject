defbd6de7092b784d96b562bb2e234f356922e8d
Outfoxeed <letourneaumorigan@gmail.com>
2020-12-21 00:31:57 +0100

    Spawn and destroy management of enemies in rooms + dialogues added to Wiloux scene

---
+++ Assets/Scripts/Runtime/RoomSpawner.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class RoomSpawner : MonoBehaviour
+{
+    public List<EnemyToSpawn> enemiesToSpawn;
+    private List<EnemyBase> enemies = new List<EnemyBase>();
+
+    #region Custom Inspector Method
+    public void HelpReferencement()
+    {
+        foreach(EnemyToSpawn enemyToSpawn in enemiesToSpawn)
+        {
+            if(enemyToSpawn.enemy != null)
+            {
+                enemyToSpawn.spawnPos = enemyToSpawn.enemy.transform.position;
+                enemyToSpawn.spawnRot = enemyToSpawn.enemy.transform.eulerAngles;
+            }
+        }
+    }
+    #endregion
+
+    #region Enemies Management
+    public void SpawnEnemies()
+    {
+        foreach(EnemyToSpawn enemyToSpawn in enemiesToSpawn)
+        {
+            EnemyBase enemy = Instantiate(enemyToSpawn.enemy, enemyToSpawn.spawnPos, Quaternion.Euler(enemyToSpawn.spawnRot));
+            enemies.Add(enemy);
+        }
+    }
+    public void DestroyEnemies()
+    {
+        foreach(EnemyBase enemy in enemies)
+        {
+            Destroy(enemy.gameObject);
+        }
+        enemies.Clear();
+    }
+    #endregion
+}
+
+[Serializable]public class EnemyToSpawn 
+{
+    public EnemyBase enemy;
+    public Vector3 spawnPos;
+    public Vector3 spawnRot;
+
+    public EnemyToSpawn(EnemyBase enemy, Vector3 spawnPos, Vector3 spawnRot)
+    {
+        this.enemy = enemy;
+        this.spawnPos = spawnPos;
+        this.spawnRot = spawnRot;
+    }
+}
