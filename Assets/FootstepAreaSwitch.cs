5e5710401a951c485c486da36eb14f6672de3da3
miguel-music <miguel.ines.music@gmail.com>
2021-01-28 20:11:12 +0000

    Area switch working for wwise stuff

---
+++ Assets/FootstepAreaSwitch.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootstepAreaSwitch : MonoBehaviour
+{
+    public string SwitchGroup = "GroundMaterial";
+    public string Switch = "Forest";
+    public string ExitSwitch = "Normal";
+    public GameObject Player;
+    public bool Debug_Enabled;
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (Debug_Enabled) { Debug.Log(Switch + "switch set"); }
+        AkSoundEngine.SetSwitch(SwitchGroup, Switch, Player);
+    }
+    private void OnTriggerExit(Collider collision)
+    {
+        if (Debug_Enabled) { Debug.Log(ExitSwitch + "switch set"); }
+        AkSoundEngine.SetSwitch(SwitchGroup, ExitSwitch, Player);
+    }
+}
+
