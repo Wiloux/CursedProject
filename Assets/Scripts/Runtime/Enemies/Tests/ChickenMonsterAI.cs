78a408dfb6372fc9d746fe16ed542892213df40f
Outfoxeed <letourneaumorigan@gmail.com>
2021-02-03 19:50:01 +0100

    Few fixes and more tests

--- Assets/Scripts/Runtime/Enemies/Tests/ChickenMonsterAI.cs
+++ Assets/Scripts/Runtime/Enemies/Tests/ChickenMonsterAI.cs
@@ -14,7 +14,7 @@ public class ChickenMonsterAI : MonoBehaviour
         Watching
     }
     private State state;
-    private IUnit unit;
+    private EnemyBaseUnit unit;
     private Transform player;
 
     [SerializeField] private EnemyProfileSO enemyProfile;
@@ -23,7 +23,7 @@ public class ChickenMonsterAI : MonoBehaviour
 
     private void Awake()
     {
-        unit = GetComponent<IUnit>();
+        unit = GetComponent<EnemyBaseUnit>();
         player = PlayerHelper.instance.transform;
         health = enemyProfile.maxHealth;
         state = State.Looking;
@@ -33,6 +33,23 @@ public class ChickenMonsterAI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        switch (state)
+        {
+            case State.Looking:
+                unit.LookForPlayer(() => state = State.Chasing);
+                break;
+            case State.Chasing:
+                unit.ChaseThePlayer(0.25f, () => state = State.Attacking);
+                break;
+            case State.Attacking:
+                unit.Attack(() => state = State.Running);
+                break;
+            case State.Running:
+                unit.RunFromPlayer(0.25f, () => state = State.Watching);
+                break;
+            case State.Watching:
+                unit.WatchThePlayer(() => state = State.Chasing);
+                break;
+        }
     }
 }
