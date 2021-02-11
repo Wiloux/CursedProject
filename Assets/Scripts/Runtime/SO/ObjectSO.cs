d13b048f08c01f4564dad6dc521d5100bebb94e3
Outfoxeed <letourneaumorigan@gmail.com>
2021-02-11 08:47:11 +0100

    Created a new ScriptableObject for future collectibles objects
    This SO contains the descriptions of the object, its type and will contain more in the future.
    I plan to use this for the inventory ui for example

---
+++ Assets/Scripts/Runtime/SO/ObjectSO.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+[CreateAssetMenu(menuName = "Scriptable Objects/ObjectInfos")]
+public class ObjectSO : ScriptableObject
+{
+    public string objectName;
+    public Sprite objectSprite;
+
+
+    public string shortDescription;
+    [TextArea(1,7)] public string longDescription;
+
+    public enum TypeOfObject
+    {
+        key,
+        clue,
+    }
+    public TypeOfObject type;
+}
+
+#if UNITY_EDITOR
+[CustomEditor(typeof(ObjectSO))]
+public class ObjectSOInspector : Editor
+{
+    string longDescription;
+    public override void OnInspectorGUI()
+    {
+        //base.OnInspectorGUI();
+
+        ObjectSO so = target as ObjectSO;
+
+        serializedObject.Update();
+
+        CreatePropertyField(nameof(so.objectName));
+        CreatePropertyField(nameof(so.objectSprite));
+
+        if(so.objectSprite != null)
+        {
+            GUILayout.Space(5);
+            Rect rect = GUILayoutUtility.GetLastRect();
+            Debug.Log(rect);
+            EditorGUI.DrawPreviewTexture(new Rect(rect.x + EditorGUIUtility.currentViewWidth/2 - so.objectSprite.texture.width/2, rect.y + 0.1f * so.objectSprite.texture.height, so.objectSprite.texture.width, so.objectSprite.texture.height), so.objectSprite.texture);
+            GUILayout.Space(so.objectSprite.texture.height * 1.2f);
+        }
+
+        CreatePropertyField(nameof(so.shortDescription));
+        CreatePropertyField(nameof(so.longDescription));
+
+        GUILayout.Space(20);
+
+        CreatePropertyField(nameof(so.type));
+        GUILayout.Space(250);
+        GUILayout.Label(" ");
+
+        serializedObject.ApplyModifiedProperties();
+    }
+
+    private void CreatePropertyField(string propertyName)
+    {
+        SerializedProperty sp = serializedObject.FindProperty(propertyName);
+        EditorGUILayout.PropertyField(sp);
+    }
+}
+#endif
